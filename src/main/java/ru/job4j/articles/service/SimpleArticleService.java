495711bc26e3ce729c35d466c5029aa71c4c6dc7
WhiteVax <WhiteVax@gmail.com>
2022-09-10 21:11:49 +0300

    2. GC. Out Of Mem. Тестовое задание. [#504790]

--- src/main/java/ru/job4j/articles/service/SimpleArticleService.java
+++ src/main/java/ru/job4j/articles/service/SimpleArticleService.java
@@ -7,7 +7,6 @@ import ru.job4j.articles.model.Word;
 import ru.job4j.articles.service.generator.ArticleGenerator;
 import ru.job4j.articles.store.Store;
 
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SimpleArticleService implements ArticleService {
@@ -25,16 +24,9 @@ public class SimpleArticleService implements ArticleService {
     public void generate(Store<Word> wordStore, int count, Store<Article> articleStore) {
         LOGGER.info("Геренация статей в количестве {}", count);
         var words = wordStore.findAll();
-
         IntStream.iterate(0, i -> i < count, i -> i + 1)
                 .peek(i -> LOGGER.info(ARTICLE_NUMBER, i))
                 .mapToObj((x) -> articleGenerator.generate(words))
                 .forEach(articleStore :: save);
-
-//        var articles = IntStream.iterate(0, i -> i < count, i -> i + 1)
-//                .peek(i -> LOGGER.info(ARTICLE_NUMBER, i))
-//                .mapToObj((x) -> articleGenerator.generate(words))
-//                .collect(Collectors.toList());
-//        articles.forEach(articleStore::save);
     }
 }
